21160f9cdaf5f941755e0173a4343ed5eef52999
cassia11 <cassiagomes0102@gmail.com>
2019-10-16 14:06:41 -0300

    02/@eduardaschuler, @jefersoncruz3, @cassia11: cria getters e setters dos atributos da classe estagio

--- webservice/src/main/java/com/thoughtworks/aceleradora/dominio/Estagio.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/dominio/Estagio.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.aceleradora.dominio;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Estagio {
@@ -8,4 +9,35 @@ public class Estagio {
     private String solucao;
     private String nome;
 
+    public Estagio() {
+        perguntas = new ArrayList<>();
+    }
+
+    public int getOrdem() {
+        return ordem;
+    }
+
+    public void setOrdem(int ordem) {
+        this.ordem = ordem;
+    }
+
+    public List<Pergunta> getPerguntas() {
+        return perguntas;
+    }
+
+    public String getSolucao() {
+        return solucao;
+    }
+
+    public void setSolucao(String solucao) {
+        this.solucao = solucao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
