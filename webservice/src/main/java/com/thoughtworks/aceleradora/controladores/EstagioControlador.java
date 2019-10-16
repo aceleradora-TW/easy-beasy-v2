f43a76f8a9713b6347889cc9343a76970ce2ff6f
cassia11 <cassiagomes0102@gmail.com>
2019-10-16 14:39:34 -0300

    02/@eduardaschuler, @jefersoncruz3, @cassia11: cria metodo na controller que retorna uma solucao

--- webservice/src/main/java/com/thoughtworks/aceleradora/controladores/EstagioControlador.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/controladores/EstagioControlador.java
@@ -4,6 +4,7 @@ import com.thoughtworks.aceleradora.dominio.Estagio;
 import com.thoughtworks.aceleradora.dominio.Pergunta;
 import com.thoughtworks.aceleradora.servicos.EstagioServico;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,15 @@ public class EstagioControlador {
     public EstagioControlador(EstagioServico estagioServico) {
         this.estagioServico = estagioServico;
     }
+
     @GetMapping(path = "/solucao")
-    public List<Estagio> pegaSolucao() {
+    public List<Estagio> pegaListaDeSolucao() {
         return estagioServico.pegarSolucoes();
     }
+
+    @GetMapping(path = "/{indice}")
+    public Estagio pegaSolucao(@PathVariable("indice") int indice) {
+        return estagioServico.pegaSolucao(indice);
+    }
+
 }
