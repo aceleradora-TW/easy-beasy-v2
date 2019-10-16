8c03cdc57ccf5129728dd08c1e1d1de9412c66f2
cassia11 <cassiagomes0102@gmail.com>
2019-10-16 14:46:45 -0300

    02/@eduardaschuler, @jefersoncruz3, @cassia11: refatora classe estagio

--- webservice/src/main/java/com/thoughtworks/aceleradora/dominio/Estagio.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/dominio/Estagio.java
@@ -3,48 +3,15 @@ package com.thoughtworks.aceleradora.dominio;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Estagio {
-    private int ordem;
-    private List<Pergunta> perguntas;
     private String solucao;
-    private String nome;
 
-    public Estagio() {
-        perguntas = new ArrayList<>();
-    }
     @JsonCreator
     public Estagio(@JsonProperty("solucao") String solucao) {
         this.solucao = solucao;
     }
 
-    public int getOrdem() {
-        return ordem;
-    }
-
-    public void setOrdem(int ordem) {
-        this.ordem = ordem;
-    }
-
-    public List<Pergunta> getPerguntas() {
-        return perguntas;
-    }
-
     public String getSolucao() {
         return solucao;
-    }
-
-    public void setSolucao(String solucao) {
-        this.solucao = solucao;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 }
