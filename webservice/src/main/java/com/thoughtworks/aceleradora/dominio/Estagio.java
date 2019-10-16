01d82d75d9c2b3d85618cb11228e30e3ceffc435
cassia11 <cassiagomes0102@gmail.com>
2019-10-16 14:04:56 -0300

    02/@eduardaschuler, @jefersoncruz3, @cassia11: cria classe estagio e seus atributos

---
+++ webservice/src/main/java/com/thoughtworks/aceleradora/dominio/Estagio.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.aceleradora.dominio;
+
+import java.util.List;
+
+public class Estagio {
+    private int ordem;
+    private List<Pergunta> perguntas;
+    private String solucao;
+    private String nome;
+
+}
