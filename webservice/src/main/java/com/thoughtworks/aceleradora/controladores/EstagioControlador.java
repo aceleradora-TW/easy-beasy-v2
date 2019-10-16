2e127591f4408301190d66a0b2ec28c4d0aa9fd4
cassia11 <cassiagomes0102@gmail.com>
2019-10-16 14:30:56 -0300

    02/@eduardaschuler, @jefersoncruz3, @cassia11: cria metodo na controller de estagio que retorna uma lista de solucao

--- webservice/src/main/java/com/thoughtworks/aceleradora/controladores/EstagioControlador.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/controladores/EstagioControlador.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.aceleradora.controladores;
 
+import com.thoughtworks.aceleradora.dominio.Estagio;
+import com.thoughtworks.aceleradora.dominio.Pergunta;
 import com.thoughtworks.aceleradora.servicos.EstagioServico;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/estagio")
@@ -12,5 +17,9 @@ public class EstagioControlador {
 
     public EstagioControlador(EstagioServico estagioServico) {
         this.estagioServico = estagioServico;
+    }
+    @GetMapping(path = "/solucao")
+    public List<Estagio> pegaSolucao() {
+        return estagioServico.pegarSolucoes();
     }
 }
