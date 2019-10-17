9d0662082fbf840a2fa898571b060c2c7e98ea99
BrayanArgradem <brayancosta299@gmail.com>
2019-10-17 15:52:36 -0300

    02/entregar-solucao-frontend/@adrioM @braynargradem @jefersoncruz3 @queiroxzz: cria metodo para pegar dados do request para renderizar na tela

--- webapp/src/components/Home.vue
+++ webapp/src/components/Home.vue
@@ -1,47 +1,34 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<template> 
+<div> 
+  <h1>{{text}}</h1>
+  <p>olaaaaaaaah</p>
+  <h1>{{solutionTela}}</h1>
+</div>
+ 
 </template>
 
 <script>
 import service from '../services/pessoas.service'
+import solution from '../services/solution'
+
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   props: {
     msg: String
   },
+  data: () => ({
+    text:"Hello",
+    solutionTela: ""
+  }),
 
   created() {
-    service.getPessoas().then(r => {
+    service.getCoco().then(r => {
       // eslint-disable-next-line
       console.log(r);
+      
+    })
+    solution.solution().then(response =>{
+      this.solutionTela = response.data.solution
     })
   }
 }
