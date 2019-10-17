8723cacb5bec0e0bf565c60771658b8650e6668e
cassia11 <cassiagomes0102@gmail.com>
2019-10-17 13:36:19 -0300

    03/@gabrierabelo, @evertonfernandes93, @franciscogferreira e @cassia11: Cria header do diagnostico

---
+++ webapp/src/components/Diagnosis.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <b-navbar toggleable="lg">
+    <h2>Diagnóstico</h2>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Diagnosis'
+}
+</script>
+
+<style scoped>
+.navbar {
+    justify-content: center;
+    background-color: #00d0c8;
+    padding: 0 15px;
+}
+h2 {
+    color:#2fc0d5;
+}
+</style>
--- webapp/src/main.js
+++ webapp/src/main.js
@@ -6,6 +6,9 @@ import icons from './utils/icons'
 import BootstrapVue from 'bootstrap-vue'
 import store from './store/'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+//import './custom.scss'
 
 import './assets/scss/config/vendors.scss'
 
