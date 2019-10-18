7eceff6b9b282c6e23bcd944bcc21dfc8bd33051
gabrierabelo <gabriel.almeida97@gmail.com>
2019-10-18 15:40:46 -0300

    03/@gabrierabelo, @evertonfernandes93, @franciscogferreira: finaliza a tela de diagnostico conforme mockup

--- webapp/src/components/Diagnosis.vue
+++ webapp/src/components/Diagnosis.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <b-navbar toggleable="lg">
+    <header>
       <h2>Diagnóstico</h2>
-    </b-navbar>
+    </header>
     <div class="chat">
-      <div class="container">
-        <p class="question">Você ta bem?</p>
-        <div class="answer">
-          <b-button id="answerYes">Sim</b-button>
-          <b-button id="answerNo">Não</b-button>
-        </div>
+      <p id="pergunta">Você tem uma organização de tempo e tarefas?</p>
+      <p id="resp">Não.</p>
+      <p id="pergunta">Você geralmente esquece de coisas que precisam ser feitas?</p>
+      <p id="resp">Não.</p>
+      <p id="pergunta">Você organiza a semana em tarefas e metas a serem cumpridas?</p>
+      
+    </div>
+    <div class="footer">
+      <div class="answer">
+        <b-button squared id="answerBtn">Sim</b-button>
+        <b-button squared id="answerBtn">Não</b-button>
       </div>
     </div>
   </div>
@@ -22,26 +27,44 @@ export default {
 </script>
 
 <style scoped>
+header {
+  justify-content: center;
+  background-color: #e2e2e2;
+  padding: 5px 15px;
+}
+h2 {
+  color: #cccccc;
+}
+.chat {
+  background-color: #ffffff;
+  position: fixed;
+  width: 100%;
+  overflow: auto;
+  padding: 2rem;
+}
+.footer {
+  position: fixed;
+  bottom: 2rem;
+  width: 100%;
+}
 .answer {
   display: flex;
   justify-content: space-between;
   max-width: 200px;
   margin: 0 auto;
+  padding: 0.5rem;
+  bottom: 10px;
 }
-.answerYes{
-  margin-left: 10px;
-  margin-right: 10px;
+#pergunta {
+  text-align: left;
+  color: #b1b1b1;
 }
-.navbar {
-  justify-content: center;
-  background-color: #00d0c8;
-  padding: 5px 15px;
+#resp {
+  text-align: right;
+  color: #636363;
 }
-.chat {
-  text-align: center;
-}
-
-h2 {
-  color: #2fc0d5;
+#answerBtn{
+  background-color: #2fc0d5;
+  border-color: #2fc0d5;
 }
 </style>
