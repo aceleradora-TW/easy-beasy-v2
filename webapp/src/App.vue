b0723d83143316d07b99e5718515134ef4fe5e9c
stefanikopp <stefani.kopp@acad.pucrs.br>
2019-10-21 18:18:10 -0300

    02/entrega-sulucao-frontend/ @adrioa @evertonfernandes93 @queirxzz : Criando a solutions em modules

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <Solution/>
   </div>
 </template>
 
 <script>
 
+import Solution from '@/modules/Solutions/Solutions.vue'
+
 export default {
   name: 'app',
-  components: {}
+  components: {
+      Solution
+  }
 }
 </script>
 
