382228f28229d76b0ce105a49338cfb0046e9d21
BrayanArgradem <brayancosta299@gmail.com>
2019-10-21 10:28:04 -0300

    03/@ingridDuarte, @FranciscoGFerreira, @gabrieRabelo, @BrayanArgradem: Adicona SASS e troca id por class

--- webapp/src/components/Diagnosis.vue
+++ webapp/src/components/Diagnosis.vue
@@ -4,17 +4,16 @@
       <h2>Diagnóstico</h2>
     </header>
     <div class="chat">
-      <p id="pergunta">Você tem uma organização de tempo e tarefas?</p>
-      <p id="resp">Não.</p>
-      <p id="pergunta">Você geralmente esquece de coisas que precisam ser feitas?</p>
-      <p id="resp">Não.</p>
-      <p id="pergunta">Você organiza a semana em tarefas e metas a serem cumpridas?</p>
-      
+      <p class="pergunta">Você tem uma organização de tempo e tarefas?</p>
+      <p class="resp">Não.</p>
+      <p class="pergunta">Você geralmente esquece de coisas que precisam ser feitas?</p>
+      <p class="resp">Não.</p>
+      <p class="pergunta">Você organiza a semana em tarefas e metas a serem cumpridas?</p>
     </div>
     <div class="footer">
       <div class="answer">
-        <b-button squared id="answerBtn">Sim</b-button>
-        <b-button squared id="answerBtn">Não</b-button>
+        <b-button squared class="answerBtn">Sim</b-button>
+        <b-button squared class="answerBtn">Não</b-button>
       </div>
     </div>
   </div>
@@ -26,7 +25,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
   justify-content: center;
   background-color: #e2e2e2;
@@ -39,32 +38,38 @@ h2 {
   background-color: #ffffff;
   position: fixed;
   width: 100%;
-  overflow: auto;
+  overflow-y: hidden;
   padding: 2rem;
+
+
+  .pergunta {
+    text-align: left;
+    color: #b1b1b1;
+  }
+
+  .resp {
+    text-align: right;
+    color: #636363;
+  }
 }
 .footer {
   position: fixed;
   bottom: 2rem;
   width: 100%;
-}
-.answer {
-  display: flex;
-  justify-content: space-between;
-  max-width: 200px;
-  margin: 0 auto;
-  padding: 0.5rem;
-  bottom: 10px;
-}
-#pergunta {
-  text-align: left;
-  color: #b1b1b1;
-}
-#resp {
-  text-align: right;
-  color: #636363;
-}
-#answerBtn{
-  background-color: #2fc0d5;
-  border-color: #2fc0d5;
+  background-color: #ffffff;
+
+  .answer {
+    display: flex;
+    justify-content: space-between;
+    max-width: 200px;
+    margin: 0 auto;
+    padding: 0.5rem;
+    bottom: 10px;
+
+    .answerBtn {
+      background-color: #2fc0d5;
+      border-color: #2fc0d5;
+    }
+  }
 }
 </style>
