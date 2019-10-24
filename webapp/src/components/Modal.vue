7ae1a354c0c7e4c591b6b3b18cbab45d18ce2bfd
cassia11 <cassiagomes0102@gmail.com>
2019-10-24 15:15:43 -0300

    19/@ingridDuarte, @JefersonCruz3: Cria modal simples

---
+++ webapp/src/components/Modal.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <b-button v-b-modal.modal>NPS</b-button>
+
+  <b-modal id="modal" >
+    <p>O quanto você recomendaria este diagnóstico para um amigo?</p>
+  </b-modal>
+
+</div>
+</template>
+
+<script>
+  export default {
+    name: "Modal"
+  };
+</script>
