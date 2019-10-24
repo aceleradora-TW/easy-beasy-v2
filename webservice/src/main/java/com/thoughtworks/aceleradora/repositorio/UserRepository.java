3b1727903c53c2c5ba5bf3fcfc8e2ff100352819
EduardaSchuler <dudaschuler2011@gmail.com>
2019-10-24 13:41:10 -0300

    20/@EduardaSchuler, @nicoledbianchin: Cria Lista de dados do usuario

---
+++ webservice/src/main/java/com/thoughtworks/aceleradora/repositorio/UserRepository.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.aceleradora.repositorio;
+
+import com.thoughtworks.aceleradora.dominio.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class UserRepository {
+    private ArrayList<User> userData = new ArrayList<>();
+
+    private void addUser(User user){
+        userData.add(user);
+    }
+}
