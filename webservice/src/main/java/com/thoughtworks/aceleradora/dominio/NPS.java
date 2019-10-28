7f93ba9c017a697100d2fda0ce031e9f47b1a783
EvertonFernandes93 <evertonoliveirafernandes@outlook.com>
2019-10-28 09:55:53 -0300

    23/@Cassia11, @EvertonFernandes93: Cria classe do NPS

---
+++ webservice/src/main/java/com/thoughtworks/aceleradora/dominio/NPS.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.aceleradora.dominio;
+
+public class NPS {
+    private long id;
+    private int note;
+    private String descriptionNote;
+
+    public NPS() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    public String getDescriptionNote() {
+        return descriptionNote;
+    }
+
+}
