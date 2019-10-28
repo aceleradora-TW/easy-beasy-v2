6701689d90b0de2d8d5a0a90d6523de643d9787f
cassia11 <cassiagomes0102@gmail.com>
2019-10-28 15:22:57 -0300

    23/@evertonfernandes93, @cassia11: Refatora migration do nps

--- webservice/src/main/resources/db/migrations/V3__Create_net_promoter_score_table.sql
+++ webservice/src/main/resources/db/migrations/V3__Create_net_promoter_score_table.sql
@@ -1,7 +1,7 @@
-CREATE TABLE nps (
+CREATE TABLE net_promoter_score (
   id serial primary key,
-  note varchar(255),
-  description varchar(255),
+  score int,
+  comments varchar(255),
   id_enterpreneur bigint not null,
   FOREIGN KEY (id_enterpreneur) REFERENCES enterpreneur(id)
 );
