d2c332e9cc7af3bb02f0840e6db04d3492195be7
EvertonFernandes93 <evertonoliveirafernandes@outlook.com>
2019-10-28 09:50:14 -0300

    23/@Cassia11, @EvertonFernandes93: Cria classe do empreendedor

---
+++ webservice/src/main/java/com/thoughtworks/aceleradora/dominio/Enterprising.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.aceleradora.dominio;
+
+public class Enterprising {
+    private long id;
+    private String name;
+    private String email;
+
+    public Enterprising() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
