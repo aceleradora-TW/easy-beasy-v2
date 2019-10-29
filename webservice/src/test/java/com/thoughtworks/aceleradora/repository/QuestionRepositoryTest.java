c1a376d460b9963d3b311af9590dd9a71714eb5a
gabrierabelo <gabriel.almeida97@gmail.com>
2019-10-29 13:56:32 -0300

    expedite/@gabrierabelo, @jefersoncruz3: corrige teste unitário para retorno de erro

--- webservice/src/test/java/com/thoughtworks/aceleradora/repository/QuestionRepositoryTest.java
+++ webservice/src/test/java/com/thoughtworks/aceleradora/repository/QuestionRepositoryTest.java
@@ -2,11 +2,16 @@ package com.thoughtworks.aceleradora.repository;
 
 import com.thoughtworks.aceleradora.domain.Question;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.List;
 
 public class QuestionRepositoryTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void shouldReturnOKIfISaveAValidQuestion() {
@@ -22,9 +27,11 @@ public class QuestionRepositoryTest {
         Assert.assertEquals(questionExpected.getDescription(), questionResult.getDescription());
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void shouldReturnErrorIfISaveAQuestionWithouDescription() {
         Question questionExpected = new Question( null);
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("The question description cannot be null.");
 
         QuestionRepository questionRepository = new QuestionRepository();
         questionRepository.addQuestion(questionExpected);
