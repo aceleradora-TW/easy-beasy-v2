dcf16f14cefaf598ed5f12c820bce9a3a145df86
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-10-30 15:00:45 -0300

    19/@ingridduarte, @nicoledbianchin: cria relações entre user e nps

--- webservice/src/main/java/com/thoughtworks/aceleradora/domain/NetPromoterScore.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/domain/NetPromoterScore.java
@@ -1,11 +1,24 @@
 package com.thoughtworks.aceleradora.domain;
 
 import org.hibernate.annotations.Entity;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity
 public class NetPromoterScore {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private long id;
+
     private int score;
     private String comments;
+
+    @Column(name = "id_users")
+    @OneToOne
+    private User user;
 
     public long getId() {
         return id;
