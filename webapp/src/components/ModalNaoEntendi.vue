36b865c48129ce691e11a4a674da8173d9717364
queiroxzz <rafaelaqueiroz181@gmail.com>
2019-10-30 11:53:32 -0300

    30/ @Queiroxzz @cassia11:  Inicia modal 'não entendi'

---
+++ webapp/src/components/ModalNaoEntendi.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <b-button class="answer-btn" id="show-btn" @click="$bvModal.show('bv-modal-example')">Nao entendi</b-button>
+
+    <b-modal id="bv-modal-example" hide-footer>
+        <template v-slot:modal-title>
+            Titulo da area
+        </template>
+        <div class="d-block text-center">
+            <h3>Explicação da pergunta</h3>
+        </div>
+        <b-button class="answer-btn mt-20" block @click="$bvModal.hide('bv-modal-example')">Entendi</b-button>
+    </b-modal>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ModalNaoEntendi"
+    }
+</script>
+
+<style scoped>
+
+</style>
