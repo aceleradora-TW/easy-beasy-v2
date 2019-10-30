0ac21c71ae5dd8f2cefe04cf189f10e14eb20da9
4305710 <riguel.figueiro@thoughtworks.com>
2019-10-30 14:18:52 -0300

    20/coleta-dados-front [@rfigueiro] correçao de tela no serviço

--- webapp/src/components/User.vue
+++ webapp/src/components/User.vue
@@ -1,30 +1,32 @@
 <template>
-  <div>
+  <b-container>
+    <b-row>
       <b-input v-model="$data.user.name"></b-input>
       <b-input v-model="$data.user.email"></b-input>
       <b-button v-on:click="save($data.user)">Enviar</b-button>
-  </div>
+    </b-row>
+  </b-container>
 </template>
 ​
 <script>
 import userService from "@/services/user.service.js"
 
 export default {
-    name: "User",
-    data: () => ({
-        user: {
-            name: "",
-            email: ""
-        }
-    }),
-    methods: {
-        save: (user) => {
-        debugger;
-        userService.save(user)
-                        .then(response => alert("Usuário salvo com sucesso!"))
-                        .catch(error => alert("Algo deu errado!"))
-        }
+  name: "User",
+  data: () => ({
+    user: {
+      name: "",
+      email: ""
     }
+  }),
+  methods: {
+    save: (user) => {
+      debugger;
+      userService.save(user)
+        .then(response => alert(`Usuário salvo com sucesso! ${response.status}`))
+        .catch(error => alert(`Algo deu errado! ${error}`))
+    }
+  }
 }
 </script>
 ​
