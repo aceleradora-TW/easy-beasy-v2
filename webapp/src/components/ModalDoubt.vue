9295434b1ecbbb103652bfe421150fcbe7906f94
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-11-01 11:22:56 -0300

    30/@ingridDuarte, @queiroxzz: Padroniza layout dentro da modal

--- webapp/src/components/ModalDoubt.vue
+++ webapp/src/components/ModalDoubt.vue
@@ -1,25 +1,45 @@
 <template>
-    <div>
-        <b-button class="dontUnderstand-btn" id="show-btn" @click="$bvModal.show('bv-modal-example')">Não entendi</b-button>
+  <div>
+    <b-button
+      class="dontUnderstand-btn"
+      id="show-btn"
+      v-on:click="$bvModal.show('bv-modal-example')"
+    >Não entendi</b-button>
 
     <b-modal id="bv-modal-example" hide-footer>
-        <template v-slot:modal-title>
-            Titulo da area
-        </template>
-        <div class="d-block text-center">
-            <h3>Explicação da pergunta</h3>
-        </div>
-        <b-button class="answer-btn mt-20" block @click="$bvModal.hide('bv-modal-example')">Entendi</b-button>
+      <template v-slot:modal-title>Explicação Financeiro</template>
+      <div class="text">
+        <h3>Explicação</h3>
+      </div>
+      <b-container fluid>
+        <b-row class="mb-3">
+          <b-col md="1.5" class="ml-md-auto">
+            <b-button
+              squared
+              type="submit"
+              v-on:click="$bvModal.hide('modal')"
+              class="answer-btn mt-20"
+            >Entendi</b-button>
+          </b-col>
+        </b-row>
+      </b-container>
     </b-modal>
-    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "ModalDoubt"
-    }
+export default {
+  name: "ModalDoubt"
+};
 </script>
 
 <style scoped>
-
+.answer-btn:hover{
+  background-color: #2fc0d5;
+  border-color: #2fc0d5;
+}
+.answer-btn {
+  background-color: #2fc0d5;
+  border-color: #2fc0d5;
+}
 </style>
