a71db2cb3696bc98b24561c55dc2e1899dc8110b
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-01 10:24:55 -0300

    08/@eduardaschuler,@FranciscoGFerreira: cria stage

---
+++ webservice/src/main/java/com/thoughtworks/aceleradora/domain/Stage.java
@@ -0,0 +1,36 @@
+package com.thoughtworks.aceleradora.domain;
+
+import java.util.List;
+
+public class Stage {
+    private List<Question> questions;
+    private String solution;
+    private String name;
+
+    public Stage() {
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
