39b04a695ab5dc3690e0ab84aba21f68d0925057
cassia11 <cassiagomes0102@gmail.com>
2019-11-01 13:57:34 -0300

    19/@cassia11, @nicoledbianchin: cria metodo post na controller

--- webservice/src/main/java/com/thoughtworks/aceleradora/controller/NetPromoterScoreController.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/controller/NetPromoterScoreController.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.aceleradora.controller;
 
+import com.thoughtworks.aceleradora.domain.NetPromoterScore;
 import com.thoughtworks.aceleradora.service.NetPromoterScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class NetPromoterScoreController {
@@ -12,4 +17,12 @@ public class NetPromoterScoreController {
     public NetPromoterScoreController(NetPromoterScoreService netPromoterScoreService) {
         this.netPromoterScoreService = netPromoterScoreService;
     }
+    @PostMapping
+    public ResponseEntity<?> saveScore(@RequestBody NetPromoterScore score) {
+        System.out.println(">>>>>>>>>> " + score.getComments());
+        System.out.println(">>>>>>>>>> " + score.getScore());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
+
