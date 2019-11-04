73d1a242ed819996f2e41fc4bc60022e25cd6764
nicoledbianchin <bianchin.nicole77@gmail.com>
2019-11-04 09:47:17 -0300

    19/@nicoledbianchin, @adriom: adiciona caminho request mapping

--- webservice/src/main/java/com/thoughtworks/aceleradora/controller/NetPromoterScoreController.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/controller/NetPromoterScoreController.java
@@ -5,13 +5,15 @@ import com.thoughtworks.aceleradora.service.NetPromoterScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/net-promoter-score")
 public class NetPromoterScoreController {
-    NetPromoterScoreService netPromoterScoreService;
+    private NetPromoterScoreService netPromoterScoreService;
 
     @Autowired
     public NetPromoterScoreController(NetPromoterScoreService netPromoterScoreService) {
