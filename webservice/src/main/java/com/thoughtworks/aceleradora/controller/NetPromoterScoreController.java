fcb1d73bda2457424efe2c84f3010ef05aeb4e05
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-04 15:46:40 -0300

    19/@nicoledbianchin, @adriom: retira imports e anotações nao usadas

--- webservice/src/main/java/com/thoughtworks/aceleradora/controller/NetPromoterScoreController.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/controller/NetPromoterScoreController.java
@@ -2,9 +2,6 @@ package com.thoughtworks.aceleradora.controller;
 
 import com.thoughtworks.aceleradora.domain.NetPromoterScore;
 import com.thoughtworks.aceleradora.service.NetPromoterScoreService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class NetPromoterScoreController {
     private NetPromoterScoreService netPromoterScoreService;
 
-    @Autowired
     public NetPromoterScoreController(NetPromoterScoreService netPromoterScoreService) {
         this.netPromoterScoreService = netPromoterScoreService;
     }
