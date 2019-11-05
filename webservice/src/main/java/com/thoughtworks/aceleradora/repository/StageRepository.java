69397a9ad52baa42c61456bdd03573e1bc24d0d3
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-05 13:49:38 -0300

    8/@nicoledbianchin, @jefersoncruz3: cria service do estágio e inicia migração de logica

--- webservice/src/main/java/com/thoughtworks/aceleradora/repository/StageRepository.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/repository/StageRepository.java
@@ -1,36 +1,27 @@
 package com.thoughtworks.aceleradora.repository;
 
 import com.thoughtworks.aceleradora.domain.Stage;
-import org.springframework.util.StringUtils;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+@Repository
 public class StageRepository {
-
-
 
     private static ArrayList<Stage> stages = new ArrayList<>(Arrays.asList(
             new Stage("naum", 1, "explicatione"),
             new Stage("naum2", 2, "explicatwo")));
 
-    public void addStage(Stage stage) {
-        if(StringUtils.isEmpty(stage.getSolution())) {
-            throw new IllegalArgumentException("The stage solution cannot be null.");
-        }else if(StringUtils.isEmpty(stage.getDoubt())){
-            throw new IllegalArgumentException("The stage doubt cannot be null.");
-        }
-        for (Stage auxStage: stages) {
-            if (stage.getNumber() == auxStage.getNumber()){
-                throw new IllegalArgumentException("The stage number already exist.");
-            }
-        }
+    public List<Stage> save(Stage stage) {
         stages.add(stage);
+        return stages;
     }
 
     public Stage deleteStage(int number) {
-        for (Stage auxStage: stages) {
-            if (auxStage.getNumber() == number){
+        for (Stage auxStage : stages) {
+            if (auxStage.getNumber() == number) {
                 stages.remove(auxStage);
                 return auxStage;
             }
---
+++ webservice/src/main/java/com/thoughtworks/aceleradora/service/StageService.java
@@ -0,0 +1,36 @@
+package com.thoughtworks.aceleradora.service;
+
+import com.thoughtworks.aceleradora.domain.Stage;
+import com.thoughtworks.aceleradora.repository.StageRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+@Service
+public class StageService {
+    private StageRepository repository;
+
+    public StageService(StageRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Stage> save (Stage stage){
+        return repository.save(stage);
+    }
+
+
+    public void saveVerification(Stage stage) {
+        if (StringUtils.isEmpty(stage.getSolution())) {
+            throw new IllegalArgumentException("The stage solution cannot be null.");
+        } else if (StringUtils.isEmpty(stage.getDoubt())) {
+            throw new IllegalArgumentException("The stage doubt cannot be null.");
+        }
+        for (Stage auxStage : repository.getStages()) {
+            if (stage.getNumber() == auxStage.getNumber()) {
+                throw new IllegalArgumentException("The stage number already exist.");
+            }
+        }
+        save(stage);
+    }
+}
