d17e0032e46cddb69078bb5a642bcf37add83f8b
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-05 11:53:16 -0300

    8/@nicoledbianchin, @jefersoncruz3: adiciona teste de repositorio com numero repetido

--- webservice/src/test/java/com/thoughtworks/aceleradora/repository/StageRepositoryTest.java
+++ webservice/src/test/java/com/thoughtworks/aceleradora/repository/StageRepositoryTest.java
@@ -2,11 +2,18 @@ package com.thoughtworks.aceleradora.repository;
 
 import com.thoughtworks.aceleradora.domain.Stage;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.List;
 
 public class StageRepositoryTest {
+
+    @Rule
+    public ExpectedException
+    expectedException = ExpectedException.none();
+
     @Test
     public void shouldReturnOKIfAddValidStage(){
         Stage stage = new Stage("solution", 5, "doubt");
@@ -23,7 +30,14 @@ public class StageRepositoryTest {
 
     @Test
     public void shouldTrowExceptionIfStageNumberAlredyExists(){
-        Stage stage = new Stage ("solution", 2, "doubt");
+        Stage stage = new Stage ("solution", 3, "doubt");
+        Stage stageRepeated = new Stage("solution not repeated", 3, "doubt not repeated");
 
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("The stage number already exist.");
+
+        StageRepository stageRepository = new StageRepository();
+        stageRepository.addStage(stage);
+        stageRepository.addStage(stageRepeated);
     }
 }
