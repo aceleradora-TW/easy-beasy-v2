a97b7b4a7e51119c85f3150ab3587b4785438b2c
BrayanArgradem <brayancosta299@gmail.com>
2019-11-05 15:32:24 -0300

    20/@FranciscoGFerreira, @BrayanArgradem: Implemena logica de validacao de nulo em input name

--- webapp/src/components/User.vue
+++ webapp/src/components/User.vue
@@ -5,8 +5,8 @@
       <b-row>
           <b-col md="6" sm="12">
             <b-form-group label-for="user-name">
-              <b-form-input id="uer-name" type="text" v-model="user.name" :state="nameState" required placeholder="Informe seu nome"/>
-              <span v-if="!isNameValid">Nome invalido/vazio!</span>
+              <b-form-input id="user-name" type="text" v-model="user.name" :state="null" required placeholder="Informe seu nome"/>
+              <span class = "invalidName" v-if="!isNameValid">Nome invalido/vazio!</span>
             </b-form-group>
           </b-col>
       </b-row>
@@ -14,12 +14,12 @@
       <b-row>
           <b-col md="6" sm="12">
             <b-form-group label-for="user-email">
-              <b-form-input id="user-email" type="email" v-model="user.email" required placeholder="exemplo@gmail.com"/>         
+              <b-form-input id="user-email" type="email" v-model="user.email" required placeholder="exemplo@gmail.com"/>
             <span v-if="!isEmailValid">Informe seu email!</span>
             </b-form-group>
           </b-col>
       </b-row>
-      <b-button class="saveUser" variant="success" v-on:click="save(user)">Salvar</b-button>
+      <b-button class="saveUser" v-on:click="save(user)">Salvar</b-button>
     </b-form>
   </div>
 </template>
@@ -43,25 +43,32 @@ export default {
       name: { required },
       email: { required, email }
     }
-  },
-  computed:{
-    nameState(){
-      while(this.user.name === ""){
-        //debugger;
-        return false;
+  }, 
+  methods:{
+      save(user){
+      if(user.name == "" || user.email == "")
+      {
+        alert ("Por favor, preencha os seguintes campos.");
+        if(user.name == "")
+        {
+          alert ("Informe um nome.");
+        }
+        // if(user.email == email)
+        // {
+        //   alert ("Informe o email.");
+       // }
+     }
       }
+      
+   },
+  //  checkName:{
+  //   if(isNameValid){
 
-    }
-  },
-  methods: {
-    save: (user) => {
-      //e.preventDefault();
-      
-      
-      userService.save(user)
-    }
-  }
+  //   }
+  //  }
+  //   }
 }
+ 
 </script>
 <style scoped>
 .user{
