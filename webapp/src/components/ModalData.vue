c692e08a763985fdba81d6c349d4462e59bb8c40
cassia11 <cassiagomes0102@gmail.com>
2019-11-07 13:32:20 -0300

    20/@ingridDuarte, @cassia11:salva dados do usuario no banco de dados

--- webapp/src/components/ModalData.vue
+++ webapp/src/components/ModalData.vue
@@ -10,15 +10,17 @@
             <b-row>
                 <b-col md="6" sm="12">
                     <b-form-group label-for="user-name">
-                        <b-form-input id="user-name" type="text" v-model="user.name" :state="null" required placeholder="Informe seu nome"/>
-                        <span class = "invalidName" v-if="!isNameValid">Nome invalido/vazio!</span>
+                        <b-form-input id="user-name" type="text" v-model="user.name" :state="null" required
+                                      placeholder="Informe seu nome"/>
+                        <span class="invalidName" v-if="!isNameValid">Nome invalido/vazio!</span>
                     </b-form-group>
                 </b-col>
             </b-row>
             <b-row>
                 <b-col md="6" sm="12">
                     <b-form-group label-for="user-email">
-                        <b-form-input id="user-email" type="email" v-model="user.email" required placeholder="exemplo@gmail.com"/>
+                        <b-form-input id="user-email" type="email" v-model="user.email" required
+                                      placeholder="exemplo@gmail.com"/>
                         <span v-if="!isEmailValid">Informe seu email!</span>
                     </b-form-group>
                 </b-col>
@@ -26,7 +28,9 @@
             <b-container fluid>
                 <b-row class="mb-3">
                     <b-col md="1.5" class="ml-md-auto">
-                        <b-button squared type="submit" v-on:click="$bvModal.hide('modal') && save()" class="saveUser">Salvar</b-button>
+                        <b-button squared type="submit" v-on:click="$bvModal.hide('modal'), save()" class="saveUser">
+                            Salvar
+                        </b-button>
                     </b-col>
                 </b-row>
             </b-container>
@@ -35,7 +39,7 @@
 </template>
 <script>
     import userService from "@/services/user.service"
-    import { required, email } from "vuelidate/lib/validators";
+    import {required, email} from "vuelidate/lib/validators";
 
     export default {
         name: "ModalData",
@@ -49,40 +53,21 @@
         }),
         validations: {
             user: {
-                name: { required },
-                email: { required, email }
+                name: {required},
+                email: {required, email}
             }
         },
-        methods:{
-            save(){
-
-                // if(user.name == "" || user.email == "")
-                // {
-                //     alert ("Por favor, preencha os seguintes campos.");
-                //     if(user.name == "")
-                //     {
-                //         alert ("Informe um nome.");
-                //     }
-                    // if(user.email == email)
-                    // {
-                    //   alert ("Informe o email.");
-                    // }
-                // }
+        methods: {
+            save() {
                 userService.save(this.user);
             }
 
         }
-        //  checkName:{
-        //   if(isNameValid){
-
-        //   }
-        //  }
-        //   }
     }
 
 </script>
 <style scoped>
-    .user{
+    .user {
         text-align: justify;
         background-color: white;
     }
