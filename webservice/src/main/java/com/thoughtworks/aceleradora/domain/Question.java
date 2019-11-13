8565f46063d069789c1cf61e3b6c65b22d7fd727
cassia11 <cassiagomes0102@gmail.com>
2019-11-13 10:36:59 -0300

    59/@nicoledbianchin, @cassia11:adiciona anotações do hibernate

--- webservice/src/main/java/com/thoughtworks/aceleradora/domain/Question.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/domain/Question.java
@@ -3,7 +3,20 @@ package com.thoughtworks.aceleradora.domain;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "question")
 public class Question {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
     private String description;
 
     @JsonCreator
