2ddfe0cf06ac68a1a42601bad2be6cde53ce8ffe
cassia11 <cassiagomes0102@gmail.com>
2019-11-13 10:35:24 -0300

    59/@nicoledbianchin, @cassia11: refatora repositório do estágio

--- webservice/src/main/java/com/thoughtworks/aceleradora/repository/StageRepository.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/repository/StageRepository.java
@@ -1,41 +1,15 @@
 package com.thoughtworks.aceleradora.repository;
 
-import com.thoughtworks.aceleradora.domain.Question;
 import com.thoughtworks.aceleradora.domain.Stage;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public class StageRepository {
+public interface StageRepository  extends CrudRepository<Stage, Long> {
 
-
-    private ArrayList<Question> P1 = new ArrayList<>(Arrays.asList(
-            new Question("Você tem uma organização de tempo e tarefas?"),
-            new Question("Você geralmente esquece de coisas que precisam ser feitas?"),
-            new Question("Você organiza a semana em tarefas e metas a serem cumpridas?"),
-            new Question("Você utiliza alguma ferramenta ou estratégia para organizar a sua semana?")));
-
-    private List<Stage> stages = new ArrayList<>(Arrays.asList(
-            new Stage("Controle seus gastos!", 1, "hint", P1, 1L)));
-
-    public List<Stage> save(Stage stage) {
-        stages.add(stage);
-        return stages;
-    }
-
-    public Stage deleteStage(Stage stage) {
-        stages.remove(stage);
-        return stage;
-    }
-
-    public List<Stage> getStages() {
-        return stages;
-    }
-
-    public Stage getStageById(Long id) {
-        return getStages().stream().filter(stage -> stage.getId() == id).findFirst().get();
-    }
+    List<Stage> findAll();
+    Optional<Stage> findById(Long Id);
 }
