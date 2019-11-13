a618e4dd49c454ce612dd8f26cb5187da358710a
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-11-13 15:46:55 -0300

    00/@ingridduarte, @nicoledbianchin: complementa teste do stage

--- webservice/src/test/java/functional/TestTestOfTest.java
+++ webservice/src/test/java/functional/TestTestOfTest.java
@@ -16,6 +16,10 @@ public class TestTestOfTest extends BaseRestAssuredTest {
                 .statusCode(200)
                 .body("solution", equalTo("Controle seus gastos!"))
                 .body("number", equalTo(1))
-                .body("hint", equalTo("hint"));
+                .body("hint", equalTo("hint"))
+                .body("questions[0].description", equalTo("Você tem uma organização de tempo e tarefas?"))
+                .body("questions[1].description", equalTo("Você geralmente esquece de coisas que precisam ser feitas?"))
+                .body("questions[2].description", equalTo("Você organiza a semana em tarefas e metas a serem cumpridas?"))
+                .body("questions[3].description", equalTo("Você utiliza alguma ferramenta ou estratégia para organizar a sua semana?"));
     }
 }
