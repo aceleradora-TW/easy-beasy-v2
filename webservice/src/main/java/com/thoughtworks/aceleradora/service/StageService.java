c29c28b73db7ad9512b9e2a19d5ef039e10069e9
EvertonFernandes93 <evertonoliveirafernandes@outlook.com>
2019-11-18 14:55:01 -0300

    59/@jefersoncruz3, @cassia11: circleci funciona, por favor

--- webservice/src/main/java/com/thoughtworks/aceleradora/service/StageService.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/service/StageService.java
@@ -54,6 +54,7 @@ public class StageService {
     }
 
     private void validation(Stage stage) {
+        List<Stage> stages = repository.findAll();
 
         if (StringUtils.isEmpty(stage.getSolution())) {
             throw new IllegalArgumentException("The stage solution cannot be null.");
@@ -70,7 +71,7 @@ public class StageService {
             throw new IllegalArgumentException("The stage doubt cannot starts with space.");
         }
 
-        for (Stage auxStage : repository.findAll()) {
+        for (Stage auxStage : stages) {
             if (stage.getNumber() == auxStage.getNumber()) {
                 throw new IllegalArgumentException("The stage number already exist.");
             }
