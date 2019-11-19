7dc193dc50e4baf7822a6600d1d602a9c7cf0bec
cassia11 <cassiagomes0102@gmail.com>
2019-11-19 14:21:29 -0300

    59/@cassia11, @adriom:ajusta imports

--- webservice/src/main/java/com/thoughtworks/aceleradora/domain/Stage.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/domain/Stage.java
@@ -2,7 +2,13 @@ package com.thoughtworks.aceleradora.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
 import java.util.ArrayList;
 import java.util.List;
 import static javax.persistence.GenerationType.IDENTITY;
