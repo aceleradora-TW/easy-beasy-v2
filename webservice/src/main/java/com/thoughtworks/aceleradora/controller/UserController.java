0b4d2a33a722767e3119d85f6bd6948f80a42fcc
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-11-21 15:29:10 -0300

    74/@eduardaschuler, @ingridDuarte: retira imports que não estão sendo utilizados

--- webservice/src/main/java/com/thoughtworks/aceleradora/controller/UserController.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/controller/UserController.java
@@ -3,23 +3,19 @@ package com.thoughtworks.aceleradora.controller;
 import com.thoughtworks.aceleradora.domain.User;
 import com.thoughtworks.aceleradora.service.UserService;
 import com.thoughtworks.aceleradora.validators.ResponseEntityErrors;
-import com.thoughtworks.aceleradora.validators.UserValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private UserService userService;
-    private ResponseEntityErrors responseEntityErrors;
 
     public UserController(UserService userService) {
         this.userService = userService;
