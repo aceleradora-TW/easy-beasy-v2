e1d40497d37a7a9cc74426cc387a6922719286c8
brayanargradem <brayancosta299@gmail.com>
2019-11-22 14:02:10 -0300

    92/@Adriom, @BrayanArgradem: Implementa codigo do Analytics para monitorar a aplicaçao

---
+++ webapp/src/components/Analytics.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <button @click="increase">Add</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+    data() {
+        return{
+            counter: 0
+        }
+    },
+
+    methods: {
+        increase () {
+            this.counter = this.counter + 1
+
+            this.$ga.event ({
+                eventCategory: 'counter',
+                eventAction: 'increase',
+                eventValue: this.counter
+            })
+        }
+    }
+}
+</script>
--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
-import Vuelidate from "vuelidate";
+import Vuelidate from "vuelidate"
 import Eventbus from './utils/eventbus'
 import icons from './utils/icons'
 import BootstrapVue from 'bootstrap-vue'
@@ -9,10 +9,22 @@ import store from './store/'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import VueAnalytics from 'vue-analytics'
 import './assets/scss/config/vendors.scss'
 
 Vue.config.productionTip = false
+
+const isPord = process.env.NODE_ENV === 'production'
+
+Vue.use(VueAnalytics, {
+    id: 'UA-152951201-1',
+    router,
+    debug:{
+        enabled: !isPord,
+        sendHitTask: isPord
+    }
+})
+
 Vue.use(Vuex)
 Vue.use(icons)
 Vue.use(BootstrapVue)
