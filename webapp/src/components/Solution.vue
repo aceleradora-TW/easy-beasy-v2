3aba4f2f3ca55423c04f188f1f552257032318ed
gabrierabelo <gabriel.almeida97@gmail.com>
2019-11-26 13:14:09 -0300

    expedite/@gabrierabelo, @nicoledbianchin: coloca link separado da solução e linkavel

--- webapp/src/components/Solution.vue
+++ webapp/src/components/Solution.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>{{solutionScreen}}</p>
+    <p>{{solutionDescription}} <a v-bind:href=solutionLink target="_blank"> Clique aqui </a></p>
   </div>
 </template>
 
@@ -10,13 +10,31 @@ import StageService from "@/services/stage.service";
 export default {
   data: () => ({
     solutionScreen: "",
+    solutionDescription: "",
+    solutionLink: "",
     idStage: 1
   }),
   created() {
     StageService.getStageById(this.idStage).then(r => {
       const stage = r.data;
       this.solutionScreen = stage.solution;
+      this.splitSolution();
     });
+  },
+  methods: {
+    splitSolution() {
+      let splittedSolution = this.solutionScreen.split(' ');
+      console.log(splittedSolution);
+      this.solutionLink = splittedSolution[splittedSolution.length -1];
+      console.log(this.solutionLink);
+      splittedSolution.forEach(element => {
+        if(element === this.solutionLink) return;  
+        this.solutionDescription += element + " ";
+        //nao pode ter nem ponto no final
+        //talvez valha a pena criar a historia pra criar slution no banco de dados
+      });
+      console.log(this.solutionDescription)
+    }
   }
 };
 </script>
