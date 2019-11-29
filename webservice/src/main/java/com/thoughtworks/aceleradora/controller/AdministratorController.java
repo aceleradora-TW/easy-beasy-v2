9390fe92afb1d43bf16bc7dbd6e9ae81281e9e85
cassia11 <cassiagomes0102@gmail.com>
2019-11-29 15:27:28 -0300

    117/@nicoledbianchin, @cassia11: cria rota na controller para salvar um adm

--- webservice/src/main/java/com/thoughtworks/aceleradora/controller/AdministratorController.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/controller/AdministratorController.java
@@ -1,4 +1,26 @@
 package com.thoughtworks.aceleradora.controller;
 
+import com.thoughtworks.aceleradora.domain.Administrator;
+import com.thoughtworks.aceleradora.service.AdministratorService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cadastrar")
 public class AdministratorController {
+
+    private AdministratorService administratorService;
+
+    public AdministratorController(AdministratorService administratorService) {
+        this.administratorService = administratorService;
+    }
+
+    @PostMapping("/")
+    public void save(@RequestBody Administrator administrator) {
+
+        administratorService.save(administrator);
+
+    }
 }
