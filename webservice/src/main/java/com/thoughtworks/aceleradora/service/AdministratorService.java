7bb73ded806592a12f07e6f032e7e0c1e2d63c00
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-29 15:23:19 -0300

    117/@nicoledbianchin, @cassia11: cria service do administrador e metodo de salvar adm

---
+++ webservice/src/main/java/com/thoughtworks/aceleradora/service/AdministratorService.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.aceleradora.service;
+
+import com.thoughtworks.aceleradora.domain.Administrator;
+import com.thoughtworks.aceleradora.domain.User;
+import com.thoughtworks.aceleradora.repository.AdministratorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdministratorService {
+
+    private AdministratorRepository repository;
+
+    public AdministratorService(AdministratorRepository repository) {
+        this.repository = repository;
+    }
+    public Administrator save(Administrator administrator){
+        return repository.save(administrator);
+    }
+
+}
