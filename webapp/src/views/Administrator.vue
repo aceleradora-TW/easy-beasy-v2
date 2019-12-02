64c99725d9c4cc8ed8981a658ac92223c1bf228d
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-12-02 15:36:48 -0300

    117/@cassia11, @brayanargradem, @nicoledbianchin: adiciona mais uma tela

---
+++ webapp/src/views/Administrator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Administrator/>
+  </div>
+</template>
+
+<script>
+import Administrator from '@/components/Administrator';
+
+export default {
+  components:{
+    Administrator,
+  }
+}
+</script>
+
+<style scoped>
+</style>
