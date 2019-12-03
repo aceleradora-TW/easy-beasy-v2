1b7a1442b8fc5836cac7b1f86eb85ac9ed0ead1d
eduardaschuler <dudaschuler2011@gmail.com>
2019-12-03 09:48:55 -0300

    117/@nicoledbianchin, @eduardaschuler: divide html em colunas e adiciona css no botao

--- webapp/src/components/Administrator.vue
+++ webapp/src/components/Administrator.vue
@@ -1,54 +1,71 @@
 <template>
   <div>
-    <b-form @submit="onSubmit" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email:"
-        label-for="input-1"
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="exemplo@email.com"
-        ></b-form-input>
-      </b-form-group>
 
-      <b-form-group id="input-group-2" label="Username:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-        ></b-form-input>
-      </b-form-group>
+    <b-col>
+      <p>Cabeçalho</p>
+    </b-col>
 
-       <b-form-group id="input-group-3" label="Password:" label-for="input-2">
-        <b-form-input
-          id="input-3"
-          v-model="form.password"
-          required
-        ></b-form-input>
-      </b-form-group>
+    <b-form @submit="onSubmit">
+      <b-col>
+        <b-form-group id="input-group-1" label="Email:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="exemplo@email.com"
+          ></b-form-input>
+        </b-form-group>
+      </b-col>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-col>
+        <b-form-group
+          id="input-group-2"
+          label="Nome do usuário:"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="form.name"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </b-col>
+
+      <b-col>
+        <b-form-group id="input-group-3" label="Senha:" label-for="input-2">
+          <b-form-input
+            id="input-3"
+            v-model="form.password"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </b-col>
+
+      <b-button class="btn" type="submit">Acessar</b-button>
     </b-form>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        form: {
-          email: '',
-          name: '',
-        },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        name: ""
       }
-    },
-    methods: {
-      },
-  }
+    };
+  },
+  methods: {}
+};
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/config/variables.scss";
+.btn {
+  background-color: $primary-color;
+  border-color: $primary-color;
+  font-family: $primary-font-family;
+}
+</style>
