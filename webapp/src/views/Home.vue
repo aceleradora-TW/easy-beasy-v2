6178c9148d860b66c392f71c755245c7ae7fe7f8
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-12-03 10:28:57 -0300

    @gabrierabelo, @mjoliveira: refatora rotas e remove rota question

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-    <ModalNps/>
     <Diagnosis/>
   </div>
 </template>
 
 <script>
 import Diagnosis from '@/components/Diagnosis'
-import ModalNps from '@/components/ModalNps'
 
 export default {
   components:{
-    Diagnosis,
-    ModalNps
+    Diagnosis
   }
 }
 </script>
