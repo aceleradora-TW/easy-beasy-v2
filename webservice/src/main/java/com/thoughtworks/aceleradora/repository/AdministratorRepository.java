47830bbe89aa603ea89b2e810b2e8a49c6a2882e
cassia11 <cassiagomes0102@gmail.com>
2019-12-05 15:57:12 -0300

    117/@nicoledbianchin, @cassia11: cria metodo para buscar username

--- webservice/src/main/java/com/thoughtworks/aceleradora/repository/AdministratorRepository.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/repository/AdministratorRepository.java
@@ -1,9 +1,12 @@
 package com.thoughtworks.aceleradora.repository;
 
 import com.thoughtworks.aceleradora.domain.Administrator;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdministratorRepository extends CrudRepository<Administrator, Long> {
+public interface AdministratorRepository extends JpaRepository<Administrator, Long> {
+    Administrator findByUsername(String username);
+
 }
