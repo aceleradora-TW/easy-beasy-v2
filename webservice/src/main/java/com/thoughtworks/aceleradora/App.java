2fedc76edcbb01cf57ea35bd22105aa69a472894
eduardaschuler <dudaschuler2011@gmail.com>
2019-12-09 10:20:41 -0300

    117/@eduardaschuler, @brayanargradem, @nicoledbianchin: exclui mudanças desnecessárias

--- webservice/src/main/java/com/thoughtworks/aceleradora/App.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/App.java
@@ -33,27 +33,4 @@ public class App {
         bean.setOrder(0);
         return bean;
     }
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-        sessionLocaleResolver.setDefaultLocale(new Locale("pt", "BR"));
-        return sessionLocaleResolver;
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource
-                = new ReloadableResourceBundleMessageSource();
-
-        messageSource.setBasename("classpath:mensagens/mensagens");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean getValidator() {
-        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-        bean.setValidationMessageSource(messageSource());
-        return bean;
-    }
 }
