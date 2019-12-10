ad3275ec1da3e4acaf39f7aa3dde5c3a24927b9e
eduardaschuler <dudaschuler2011@gmail.com>
2019-12-10 13:47:45 -0300

    117/@ingridDuarte, @eduardaschuler: remove imports que não estão sendo utilizados

--- webservice/src/main/java/com/thoughtworks/aceleradora/App.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/App.java
@@ -3,17 +3,10 @@ package com.thoughtworks.aceleradora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @SpringBootApplication
 public class App {
