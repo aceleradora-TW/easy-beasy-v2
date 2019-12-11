5bcff8e6bd4863d2eeb436b3d5b1bcb803df97bb
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-12-11 14:40:00 -0300

    117/@nicoledbianchin, @eduardaschuler, @hsangalli: faz web configuration do spring secutiry

--- webservice/src/main/java/com/thoughtworks/aceleradora/App.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/App.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -27,4 +29,8 @@ public class App {
         return bean;
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
