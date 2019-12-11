f54afd4ff4ebfd430299b58c7211d091f1d7d049
eduardaschuler <dudaschuler2011@gmail.com>
2019-12-11 09:01:34 -0300

    117/@eduardaschuler, @queiroxzz: wip

--- webservice/src/main/java/com/thoughtworks/aceleradora/service/AdministratorService.java
+++ webservice/src/main/java/com/thoughtworks/aceleradora/service/AdministratorService.java
@@ -1,8 +1,9 @@
 package com.thoughtworks.aceleradora.service;
 
 import com.thoughtworks.aceleradora.domain.Administrator;
-import com.thoughtworks.aceleradora.domain.User;
 import com.thoughtworks.aceleradora.repository.AdministratorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,10 +11,14 @@ public class AdministratorService {
 
     private AdministratorRepository repository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public AdministratorService(AdministratorRepository repository) {
         this.repository = repository;
     }
     public Administrator save(Administrator administrator){
+        administrator.setPassword(passwordEncoder.encode(administrator.getPassword()));
         return repository.save(administrator);
     }
 
