d7ec2a67471dbbbdc1fe6d247a8f3186579129c8
FranciscoGFerreira <franciscogabriellferreira@gmail.com>
2019-12-13 14:54:53 -0300

    96/@nicoledbianchin, @franciscogferreira: ajusta identaçao

--- webapp/src/components/RegisterQuestions.vue
+++ webapp/src/components/RegisterQuestions.vue
@@ -18,8 +18,7 @@
                 $v.question.questionInput.$dirty
                   ? !$v.question.questionInput.$error
                   : null
-              "
-            >
+              ">
             </b-form-input>
           </b-form-group>
         </b-col>
@@ -32,9 +31,7 @@
         <b-col>
           <b-form-group label-for="dropdown-areaDropdown">
             <select class="form-control">
-              <option value="" selected disabled
-                >---Selecione uma opção---</option
-              >
+              <option value="" selected disabled>---Selecione uma opção---</option>
               <option>opção 1</option>
               <option>opção 2</option>
             </select>
@@ -44,10 +41,9 @@
           <b-form-group label-for="dropdown-stageDropdown">
             <select class="form-control">
               <option value="" selected disabled>Selecione uma opção</option>
-              <option v-for="stage in stageList" v-bind:key="stage.number">{{
-                stage.number
-              }}</option>
-            </select>
+                <option v-for="stage in stageList"
+                v-bind:key="stage.number">{{ stage.number }}</option>
+              </select>
           </b-form-group>
         </b-col>
       </b-row>
@@ -116,7 +112,6 @@ export default {
         return;
       }
       alert("Pergunta cadastrada com sucesso!");
-      }
     }
   }
 };
